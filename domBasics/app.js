756296e37bde9ffe584f3b0906c5f273c3d83137
Pawel Kurpinski <pkurpinski@wp.pl>
2017-06-25 21:19:53 +0200

    zapis 25.06

--- domBasics/app.js
+++ domBasics/app.js
@@ -3,8 +3,8 @@ var isBlue = false;
 
 setInterval(function(){
     if (isBlue){
-        body.style.background = "powderblue";
+        body.style.background = "blue";
     } else {
-        body.style.background = "red";
+        body.style.background = "green";
     }isBlue = !isBlue;
-}, 1000);
+}, 1000)
