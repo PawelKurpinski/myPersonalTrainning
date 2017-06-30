d4deb9212f36dc15cb69f0ea25d157f56596f4db
Pawel Kurpinski <pkurpinski@wp.pl>
2017-06-30 20:39:28 +0200

    zapis 3 / 30.06

--- firstProject/app.js
+++ firstProject/app.js
@@ -1,17 +1,14 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.querySelector("#reset");
 var span1 = document.querySelector("#span1");
 var span2 = document.querySelector("#span2");
+var input = document.querySelector("input");
+var gameTarget = document.querySelector ("p span");
 var p1Score = 0;
 var p2Score = 0;
 var maxScore = 5;
 var gameOver = false;
-var resetButton = document.querySelector("#reset");
-
-resetButton.addEventListener("click", function(){
-    gameOver = true;
-    p1Score = 0;
-})
 
 p1Button.addEventListener("click", function() {
     if (!gameOver) {
@@ -34,5 +31,30 @@ p2Button.addEventListener("click", function(){
         span2.textContent = p2Score;
     }
 
-
 });
+
+resetButton.addEventListener("click", function(){
+    gameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    span1.textContent = p1Score;
+    span2.textContent = p2Score;
+    span1.classList.remove("winner");
+    span2.classList.remove("winner");
+})
+
+function reset(){
+    gameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    span1.textContent = p1Score;
+    span2.textContent = p2Score;
+    span1.classList.remove("winner");
+    span2.classList.remove("winner");
+};
+input.addEventListener("change", function(){
+    gameTarget.textContent = this.value;
+    // insted of using input all the time we can take "this"
+    maxScore = Number(this.value);
+    reset();
+})
