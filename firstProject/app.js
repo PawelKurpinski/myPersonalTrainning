aadd0e815a13dd62cf781772c249f83c286a048a
Pawel Kurpinski <pkurpinski@wp.pl>
2017-06-30 10:30:45 +0200

    30.06

--- firstProject/app.js
+++ firstProject/app.js
@@ -1,7 +1,38 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
-var reset = document.querySelector("reset");
+var span1 = document.querySelector("#span1");
+var span2 = document.querySelector("#span2");
+var p1Score = 0;
+var p2Score = 0;
+var maxScore = 5;
+var gameOver = false;
+var resetButton = document.querySelector("#reset");
 
-p1Button.addEventListener("click", function(){
-    alert("WOW!");
+resetButton.addEventListener("click", function(){
+    gameOver = true;
+    p1Score = 0;
+})
+
+p1Button.addEventListener("click", function() {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === maxScore) {
+            span1.classList.add("winner");
+            gameOver = true;
+        }
+        span1.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener("click", function(){
+    if (!gameOver){
+        p2Score++;
+        if (p2Score === maxScore) {
+            span2.classList.add("winner");
+            gameOver = true;
+        }
+        span2.textContent = p2Score;
+    }
+
+
 });
